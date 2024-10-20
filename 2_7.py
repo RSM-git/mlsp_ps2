16e6bbf30ebba1fc477a13a3354ba44dd6676a85
RSM-git <rasmus.steen.mikkelsen@hotmail.com>
2024-10-20 12:55:13 +0200

    MSE and more cleanup in 2_7.py

--- 2_7.py
+++ 2_7.py
@@ -7,12 +7,24 @@ import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
 
-from adaptive_filtering_algorithms import lms, nlms, rls
+from adaptive_filtering_algorithms import lms, nlms, rls, mse
 
-PLAY_SPEECH = 1
-PLOT_SPECTROGRAMS = 1
+PLAY_SPEECH = 0
+PLOT_SPECTROGRAMS = 0
+
+repeats = 5
 
 fs = 8000
+
+L = 64
+
+mu_lms = 0.01
+
+mu_nlms = 0.05
+delta = 1e-1
+
+rls_beta = 0.999
+rls_lambda = 1
 
 def spectrogram(signal, figure_name: str, sampling_rate: int = 8000):
     spectrogram = librosa.stft(signal, n_fft=512, hop_length=32, center=True)
@@ -32,16 +44,48 @@ lpir = loadmat("problem2_7_lpir.mat")["lpir"].flatten()
 bpir = loadmat("problem2_7_bpir.mat")["bpir"].flatten()
 hpir = loadmat("problem2_7_hpir.mat")["hpir"].flatten()
 
-noise = np.random.normal(loc=0, scale=0.2, size=len(speech))
+lms_mses, nlms_mses, rls_mses = [], [], []
 
-filtered_noise = signal.lfilter(lpir, 1, noise)
+for _ in range(repeats):
+    noise = np.random.normal(loc=0, scale=0.2, size=len(speech))
 
-noisy_speech = speech + filtered_noise
+    filtered_noise = signal.lfilter(lpir, 1, noise)
+
+    noisy_speech = speech + filtered_noise
+
+    # lms
+    noise_estimate_lms, lms_error = lms(noise, noisy_speech, L, mu_lms)
+    lms_denoised_speech = noisy_speech - noise_estimate_lms
+
+    # nlms
+    noise_estimate_nlms, nlms_error = nlms(noise, noisy_speech, L, mu_nlms, delta)
+    nlms_denoised_speech = noisy_speech - noise_estimate_nlms
+
+    # rls
+    noise_estimate_rls, rls_error = rls(noise, noisy_speech, L, rls_beta, rls_lambda)
+    rls_denoised_speech = noisy_speech - noise_estimate_rls
+
+    lms_mse, nlms_mse, rls_mse = mse(lms_denoised_speech, speech), mse(nlms_denoised_speech, speech), mse(rls_denoised_speech, speech)
+
+    lms_mses.append(lms_mse)
+    nlms_mses.append(nlms_mse)
+    rls_mses.append(rls_mse)
+
+print(f"MSE of LMS signal {np.mean(lms_mses)}")
+print(f"MSE of NLMS signal {np.mean(nlms_mses)}")
+print(f"MSE of RLS signal {np.mean(rls_mses)}")
+
 
 if PLAY_SPEECH:
     play_sound(speech)
 
     play_sound(noisy_speech)
+
+    play_sound(lms_denoisd_speech)
+
+    play_sound(nlms_denoised_speech)
+
+    play_sound(rls_denoised_speech)
 
 if PLOT_SPECTROGRAMS:
     # noisy speech
@@ -50,41 +94,13 @@ if PLOT_SPECTROGRAMS:
     # clean speech
     spectrogram(speech, "Clean speech")
 
+    # lms
+    spectrogram(lms_denoised_speech, "LMS filtered signal")
+
+    # nlms
+    spectrogram(nlms_denoised_speech, "NLMS filtered signal")
+
+    # rls
+    spectrogram(rls_denoised_speech, "RLS filtered signal")
+
     plt.show()
-
-# lms
-mu_lms = 0.001
-L = 64
-
-noise_estimate, _ = lms(noise, noisy_speech, L, mu_lms)
-
-play_sound(noisy_speech-noise_estimate)
-
-spectrogram(noisy_speech-noise_estimate, "LMS filtered signal")
-plt.show()
-
-# nlms
-
-mu_nlms = 0.005
-delta = 1e-2
-L = 64
-
-noise_estimate_nlms, _ = nlms(noise, noisy_speech, L, mu_nlms, delta)
-
-play_sound(noisy_speech-noise_estimate_nlms)
-
-spectrogram(noisy_speech-noise_estimate_nlms, "NLMS filtered signal")
-plt.show()
-
-# rls
-
-rls_beta = 0.9999
-rls_lambda = 1e2
-L = 64
-
-noise_estimate_rls, _ = rls(noise, noisy_speech, L, rls_beta, rls_lambda)
-
-play_sound(noisy_speech-noise_estimate_rls)
-
-spectrogram(noisy_speech-noise_estimate_rls, "RLS filtered signal")
-plt.show()
--- adaptive_filtering_algorithms.py
+++ adaptive_filtering_algorithms.py
@@ -87,3 +87,28 @@ def rls(x: np.ndarray, y: np.ndarray, L: int, beta: float, lambda_: float):
         P = (P - K_n @ z.T)/beta
 
     return yhat, e
+
+def mse(xhat, x):
+        # MSE Compute the (optimally scaled) mean squared error
+    # between a (noisy) signal estimate and an original (noise free) signal
+    #          _              _
+    #         |             2  |
+    # e = min | (x - a*xhat)   |
+    #      a  |_              _|
+    #
+    # Usage
+    #    e = mse(xhat, x);
+    #
+    # Inputs
+    #    xhat    Estimated signal
+    #    x       Original signal
+    # 
+    # Outputs
+    #    e       Mean squared error
+    #
+    # Copyright 2013 Mikkel N. Schmidt, mnsc@dtu.dk
+    # 2022, translated to Python by Tommy S. Alstrøm, tsal@dtu.dk
+
+    a = (x.T @ xhat) / (xhat.T @ xhat)
+    e = np.mean((x-a*xhat)**2)
+    return e
